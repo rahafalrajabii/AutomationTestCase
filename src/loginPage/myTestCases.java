b204b64e19e164feca857a4397107e8afb2ee3e2
rahafalrajabii <rahafalrajabi20.gmail.com>
2025-07-03 11:29:57 +0300

    update

---
+++ src/loginPage/myTestCases.java
@@ -0,0 +1,38 @@
+package loginPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class myTestCases {
+
+	WebDriver driver = new ChromeDriver ();
+	String theURL = "https://automationteststore.com/";
+	String SignupPage = "https://automationteststore.com/index.php?rt=account/create"; 
+	@BeforeTest
+	
+	public void mySetup () {
+		
+//		driver.get("https://automationteststore.com/");
+		driver.get(theURL);
+			
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test (priority = 1)
+	public void signup () {
+		
+
+			
+			driver.navigate().to(SignupPage);
+			
+			WebElement firstName = driver.findElement(By.xpath("//*[@id=\"AccountFrm_firstname\"]"));
+			
+			firstName.sendKeys("ahmad");
+			
+	}
+	
+	
+}
